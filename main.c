c57599765840cf669a466446396acd409c0fc3de
sukhchainn <32098299+sukhchainn@users.noreply.github.com>
2020-04-09 20:10:31 +0530

    main.c created

--- main.c
+++ main.c
@@ -1,12 +1,50 @@
 #include <stdio.h>
-#include "conio.h"
+#include "include/conio.h"
 
 #define VERSION "0.0.1"
+
+void log_warn(const char *str);
+
+void log_warn(const char *str)
+{
+    printf("\n");
+    textcolor(BLACK);
+    textbackground(YELLOW);
+    printf(str);
+    textcolor(WHITE);
+    textbackground(BLACK);
+    printf("\n");
+}
+
+void log_info(const char *str)
+{
+    printf("\n");
+    textcolor(WHITE);
+    textbackground(BLUE);
+    printf(str);
+    textcolor(WHITE);
+    textbackground(BLACK);
+    printf("\n");
+}
+
+void log_danger(const char *str)
+{
+    printf("\n");
+    textcolor(WHITE);
+    textbackground(RED);
+    printf(str);
+    textcolor(WHITE);
+    textbackground(BLACK);
+    printf("\n");
+}
 
 int main() {
     printf("Pocket-C version : %s", VERSION);
 
+    log_warn("Test Warn");
+    log_info("Test Info");
+    log_danger("Test Danger");
 
-
+    getch();
     return 0;
 }
---
+++ test.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    printf("Program start from here...\n");
+    system("main.exe");
+    return 0;
+}
